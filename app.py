6abd087e0b0ca53ba69fa852e8df3d33e7cae470
ingwang-kim <dlsrhkd615@gmail.com>
2022-11-16 07:59:22 +0900

    값을 받아서 즐겨찾기에 메뉴 추가

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
+import re
+
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 from pymongo import MongoClient
-import certifi
-
-ca = certifi.where()
-
-client = MongoClient('MongoDB url',tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ffudy0q.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.coffeeduckhu
 
-# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
-# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
-SECRET_KEY = 'SPARTA'
 
-# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
-import jwt
-
-# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
-import datetime
-
-# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
-# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
-import hashlib
-
-
-#################################
-##  HTML을 주는 부분             ##
-#################################
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('fav.html')
 
-@app.route("/users", methods=["GET"])
+
+
+@app.route("/coffees", methods=["GET"])
 def fave_get():
-    fav_num = list(db.users.find({'name':~}, {'_id': False}))
-    fav_menu = list(db.coffee.find({fav_num:coffe_id},{'_id':False}))
-    return jsonify({'coffees':fav_menu})
+    fav_list = [100,200,300,250,240,105]
+
+    # for i in fav_list:
+    #     a=(db.coffee.find({'coffee_id' : i},{'_id':False}))
+    #     print("print a " + str(a))
+    return_list = []
+    for i in fav_list:
+        for a in db.coffee.find({'coffee_id' : i},{'_id':False}):
+            return_list.append(a)
+
+    print(return_list)
+
+    return jsonify({'coffees':return_list})
+
+@app.route("/delfav", methods=["post"])
+def web_mars_add():
+    id_receive = request.form['id_give']
+
+    fav_list = db.users.delete_one({'name'id_receive})
+    return jsonify({'orders': fav_list})
+
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
