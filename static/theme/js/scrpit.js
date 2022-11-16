729f92b7360ed87803d683f6d05a4ffa8b617575
leemeo3 <nicet.shlee@gmail.com>
2022-11-17 01:28:49 +0900

    메인페이지 완성

--- static/theme/js/scrpit.js
+++ static/theme/js/scrpit.js
@@ -21,17 +21,19 @@ function ediya() {
                 let id = rows[i]['coffee_id']
                 let name = rows[i]['coffee_name']
                 let url = rows[i]['coffee_image']
+                let favorites = rows[i]['favorites']
                 let temp_html = `<div class="col-lg-4 col-6 mb-4 shuffle-item"  data-groups="[&quot;design&quot;,&quot;illustration&quot;]">
                             <div class="position-relative inner-box">
                             <div class="image position-relative ">
                             <h3><span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-                            >99+</h3></span>
+                            style="margin-top: 9px; opacity: 1; z-index: 1; transform: scale(1.1);" >${favorites}</h3></span>
                             <img src="${url}" alt="portfolio-image" class="img-fluid w-100 d-block">
                             <div class="overlay-box">
                             <div class="overlay-inner">
                             <a class="overlay-content" onclick="favorites_send();">
                             <h5 class="mb-0">${name}</h5>
-                            <p class="test">${id}</p>
+                            <p class="test" onclick="send_id(${id})" style="cursor: pointer">즐겨찾기 등록</p>
+                            <p onclick="location.href='/coffee/1.html'" style="cursor: pointer">상세페이지 이동</p>
                             </a>
                             </div>
                             </div>
@@ -52,22 +54,23 @@ function starbucks() {
         success: function (response) {
             $('#wrap').empty()
             let rows = response['starbucks']
-            let number = 2;
             for (let i = 0; i < rows.length; i++) {
                 let id = rows[i]['coffee_id']
                 let name = rows[i]['coffee_name']
                 let url = rows[i]['coffee_image']
+                let favorites = rows[i]['favorites']
                 let temp_html = `<div class="col-lg-4 col-6 mb-4 shuffle-item"  data-groups="[&quot;design&quot;,&quot;illustration&quot;]">
                             <div class="position-relative inner-box">
                             <div class="image position-relative ">
                             <h3><span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-                            style="margin-top: 9px; opacity: 1; z-index: 1; transform: scale(1.1);" >${number}++</h3></span>
+                            style="margin-top: 9px; opacity: 1; z-index: 1; transform: scale(1.1);" >${favorites}</h3></span>
                             <img src="${url}" alt="portfolio-image" class="img-fluid w-100 d-block">
                             <div class="overlay-box">
                             <div class="overlay-inner">
                             <a class="overlay-content" onclick="favorites_send();">
                             <h5 class="mb-0">${name}</h5>
-                            <p class="test">${id}</p>
+                            <p class="test" onclick="send_id(${id})" style="cursor: pointer">즐겨찾기 등록</p>
+                            <p onclick="location.href='/coffee/1.html'" style="cursor: pointer">상세페이지 이동</p>
                             </a>
                             </div>
                             </div>
@@ -93,17 +96,19 @@ function hollys() {
                 let id = rows[i]['coffee_id']
                 let name = rows[i]['coffee_name']
                 let url = rows[i]['coffee_image']
+                let favorites = rows[i]['favorites']
                 let temp_html = `<div class="col-lg-4 col-6 mb-4 shuffle-item"  data-groups="[&quot;design&quot;,&quot;illustration&quot;]">
                             <div class="position-relative inner-box">
                             <div class="image position-relative ">
                             <h3><span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-                            >99+</h3></span>
+                            style="margin-top: 9px; opacity: 1; z-index: 1; transform: scale(1.1);" >${favorites}</h3></span>
                             <img src="${url}" alt="portfolio-image" class="img-fluid w-100 d-block">
                             <div class="overlay-box">
                             <div class="overlay-inner">
                             <a class="overlay-content" onclick="favorites_send();">
                             <h5 class="mb-0">${name}</h5>
-                            <p class="test">${id}</p>
+                            <p class="test" onclick="send_id(${id})" style="cursor: pointer">즐겨찾기 등록</p>
+                            <p onclick="location.href='/coffee/1.html'" style="cursor: pointer">상세페이지 이동</p>
                             </a>
                             </div>
                             </div>
@@ -112,7 +117,6 @@ function hollys() {
                             </div>`
                 $('#wrap').append(temp_html)
             }
-
         }
     })
 }
@@ -129,17 +133,19 @@ function paikdabang() {
                 let id = rows[i]['coffee_id']
                 let name = rows[i]['coffee_name']
                 let url = rows[i]['coffee_image']
+                let favorites = rows[i]['favorites']
                 let temp_html = `<div class="col-lg-4 col-6 mb-4 shuffle-item"  data-groups="[&quot;design&quot;,&quot;illustration&quot;]">
                             <div class="position-relative inner-box">
                             <div class="image position-relative ">
                             <h3><span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-                            >99+</h3></span>
+                            style="margin-top: 9px; opacity: 1; z-index: 1; transform: scale(1.1);" >${favorites}</h3></span>
                             <img src="${url}" alt="portfolio-image" class="img-fluid w-100 d-block">
                             <div class="overlay-box">
                             <div class="overlay-inner">
                             <a class="overlay-content" onclick="favorites_send();">
                             <h5 class="mb-0">${name}</h5>
-                            <p class="test">${id}</p>
+                            <p class="test" onclick="send_id(${id})" style="cursor: pointer">즐겨찾기 등록</p>
+                            <p onclick="location.href='/coffee/1.html'" style="cursor: pointer">상세페이지 이동</p>
                             </a>
                             </div>
                             </div>
@@ -153,14 +159,24 @@ function paikdabang() {
     })
 }
 
-function favorites_send() {
+function send_id(item) {
     $.ajax({
         type: 'POST',
         url: '/favorites_send',
-        data: {coffee_id : $('.test').text()},
+        data: {coffee_id : item},
         success: function (response) {
             alert(response['msg'])
-            console.log()
+            if (response['cafe'] <= 50) {
+                hollys();
+            }else if (response['cafe'] <= 98) {
+                ediya();
+            }else if (response['cafe'] <= 247) {
+                starbucks();
+            }else if (response['cafe'] <= 342){
+                paikdabang();
+            } else {
+                starbucks();
+            }
         }
     })
 }
